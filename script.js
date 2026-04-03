e5072bda11a1ff428a3784d5a7e8c513fdb966c6
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-03 17:20:04 +0200

    aggiunta gestione del click e recupero dei dati dagli input

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const etaInput = document.querySelector("#eta");
 const bottone = document.querySelector("#calcola");
 const risultato = document.querySelector("#risultato");
 const prezzoOutput = document.querySelector("#prezzo");
+
+// al click
+bottone.onclick = function () {
+
+  // prendo valori
+  const km = parseFloat(kmInput.value);
+  const eta = parseInt(etaInput.value);
