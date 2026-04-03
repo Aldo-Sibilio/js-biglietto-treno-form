abbcdb0e90dbb23f713e2fa61663eb7e519a3ced
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-04-03 17:02:19 +0200

    Definite variabili per collegare li elementi del DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+// prendo gli elementi
+const kmInput = document.querySelector("#km");
+const etaInput = document.querySelector("#eta");
+const bottone = document.querySelector("#calcola");
+const risultato = document.querySelector("#risultato");
+const prezzoOutput = document.querySelector("#prezzo");
